477b0ffec60c74b6bf01a3f8ddb6b2998c1a9524
kingdada7 <toluwadada7@gmail.com>
2025-08-13 16:36:56 +0100

    change of port

--- api/index.js
+++ api/index.js
@@ -19,8 +19,9 @@ mongoose
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!!!");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}!!!`);
 });
 
 app.use("/api/user", userRouter);
