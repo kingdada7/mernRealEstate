333bacc65bdd9d8473b07603bc8c024faab47404
kingdada7 <toluwadada7@gmail.com>
2025-08-13 16:41:53 +0100

    change route

--- api/index.js
+++ api/index.js
@@ -19,6 +19,10 @@ mongoose
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the API!");
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}!!!`);
