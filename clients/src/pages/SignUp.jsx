5800f2f3a308f5285877cfeca7d57e97ebcdb9c8
kingdada7 <toluwadada7@gmail.com>
2025-08-18 11:58:23 +0100

    sign up completed

--- clients/src/pages/SignUp.jsx
+++ clients/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({
@@ -34,6 +35,8 @@ export default function SignUp() {
         return;
       }
       setLoading(false);
+      setError(null);
+      navigate("/sign-in");
     } catch (error) {
       setLoading(false);
       setError(error.message);
